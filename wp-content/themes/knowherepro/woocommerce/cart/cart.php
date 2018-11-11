ffc4ed3128e2731d9f409b3cdffccc58906fc0b5
Atypix <sebodion@gmail.com>
2018-11-11 14:25:16 +0100

    Update wp-content/themes/knowherepro/woocommerce/cart/cart.php

--- wp-content/themes/knowherepro/woocommerce/cart/cart.php
+++ wp-content/themes/knowherepro/woocommerce/cart/cart.php
@@ -81,7 +81,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 							$string = apply_filters('woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key);
 							$thumbnail = preg_replace('/\\<(.*?)(width="(.*?)")(.*?)(height="(.*?)")(.*?)\\>/i', '<$1$4$7>', $string);
 							
-							echo "<div width='150px'>";
+							echo "<div width='150'>";
 							if (!$product_permalink) {
 								echo $thumbnail;
 							} else {
