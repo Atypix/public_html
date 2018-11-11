031d48b5fac187a0e581ce533f21394722c8c966
Atypix <sebodion@gmail.com>
2018-11-11 14:05:11 +0100

    Update wp-content/themes/knowherepro/woocommerce/cart/cart.php

--- wp-content/themes/knowherepro/woocommerce/cart/cart.php
+++ wp-content/themes/knowherepro/woocommerce/cart/cart.php
@@ -78,7 +78,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 						<td class="product-name" data-title="<?php esc_html_e('Product', 'knowherepro'); ?>">
 							<?php
-							$thumbnail = apply_filters('small', $_product->get_image(), $cart_item, $cart_item_key);
+							$thumbnail = apply_filters('woocommerce_gallery_image_size', $_product->get_image(), $cart_item, $cart_item_key);
 
 							if (!$product_permalink) {
 								echo $thumbnail;
